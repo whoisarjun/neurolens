cab32f4e6f20861c49ef05da448dcb75299b48fd
whoisarjun <arjunpalakkal@gmail.com>
2025-07-21 16:19:12 +0800

    minor changes

--- audio/wav_generator.py
+++ audio/wav_generator.py
@@ -2,17 +2,17 @@ from TTS.api import TTS
 from pydub import AudioSegment
 import numpy as np
 
-tts = TTS(model_name="tts_models/en/vctk/vits", progress_bar=False, gpu=False)
+tts = TTS(model_name='tts_models/en/vctk/vits', progress_bar=False, gpu=False)
 
 def read(text: str, fp: str):
-    tts.tts_to_file(text=text, file_path=fp, speaker=tts.speakers[0])
+    tts.tts_to_file(text=text, file_path=fp, speaker=tts.speakers[2])
 
 def reformat(fp: str, frame_rate=8000):
     audio = AudioSegment.from_wav(fp)
     audio_8k = audio.set_frame_rate(frame_rate)
-    audio_8k.export("output.wav", format="wav")
+    audio_8k.export(fp, format="wav")
 
-def add_noise(fp: str, frame_rate=8000, scale=0.0015):
+def noise(fp: str, frame_rate=8000, scale=0.0015):
     audio = AudioSegment.from_wav(fp)
 
     samples = np.array(audio.get_array_of_samples()).astype(np.float32)
@@ -30,3 +30,15 @@ def add_noise(fp: str, frame_rate=8000, scale=0.0015):
     )
 
     noisy_audio.export(fp, format="wav")
+
+def pipeline(text: str, fp: str, frame_rate=8000, scale=0.0015, do_reformat=True, add_noise=True):
+    read(text, fp)
+    if reformat:
+        reformat(fp, frame_rate)
+    if add_noise:
+        noise(fp, frame_rate, scale)
+
+read(
+    text='Where did you go on your last holiday as a child?',
+    fp='output.wav'
+)
